2a3201144a388f1b76f231048b6d32f4c6386bdb
Samuel Kelly <samuel.kelly@gmail.com>
2014-06-12 14:19:14 -0700

    Removed semicolons :)

--- stegxt.py
+++ stegxt.py
@@ -1,19 +1,19 @@
-import nltk;
-import random;
+import nltk
+import random
 from nltk.corpus import wordnet
 
 def synset_to_word(ss):
     return ss.name.split('.')[0]
 
-orig_text = open('warofroses.txt', 'r');
-steg_text = open('warofposes.txt', 'w');
+orig_text = open('warofroses.txt', 'r')
+steg_text = open('warofposes.txt', 'w')
 
-words = nltk.word_tokenize(orig_text.read());
+words = nltk.word_tokenize(orig_text.read())
 
 for word in words:
-    synsets = wordnet.synsets(word);
+    synsets = wordnet.synsets(word)
     if len(synsets) > 0:
-        rand_idx = random.randint(0, len(synsets) - 1);
+        rand_idx = random.randint(0, len(synsets) - 1)
         rand_synset = synsets[rand_idx]
         print synset_to_word(rand_synset)
         steg_text.write(synset_to_word(rand_synset) + ' ')
